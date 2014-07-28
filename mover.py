e7094168bc9dc949d3e1884c3805d6c4816a38c6
lchsk <mjlechowski@gmail.com>
2014-07-28 18:32:34 +0200

    Added optional 3rd parameter: file extension

--- mover.py
+++ mover.py
@@ -7,10 +7,20 @@ class Config(object):
 	def __init__(self):
 		self.input_folder = sys.argv[1]
 		self.output_folder = sys.argv[2]
+		if len(sys.argv) > 3:
+		  self.extension = sys.argv[3]
+		else:
+		  self.extension = None
 		
 def create_dir(config, ext):
 	if not os.path.exists(config.output_folder + ext):
 		os.makedirs(config.output_folder + ext)
+		
+def move_internal(config, extension, f):
+  destination = config.output_folder + extension  + '/' + f
+  source = (config.input_folder + f)
+  print '\tMoving [' + f + '] from ' + source + ' to ' + destination
+  os.rename(source, destination)
 		
 def move(config):
 	files = os.listdir(config.input_folder)
@@ -21,10 +31,10 @@ def move(config):
 		if os.path.isfile(config.input_folder + f):
 			create_dir(config, extension)
 	
-			destination = config.output_folder + extension  + '/' + f
-			source = (config.input_folder + f)
-			print '\tMoving [' + f + '] from ' + source + ' to ' + destination
-			os.rename(source, destination)
+			if config.extension == extension:
+			  move_internal(config, extension, f)
+			elif config.extension == None:
+			  move_internal(config, extension, f)
 
 def main():
 	config = Config()
